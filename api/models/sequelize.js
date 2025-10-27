033f4305d8d804fd8f51a95e51df9488507667d0
QuentinCaffray <quentin.caffray@oclock.school>
2025-10-27 16:25:15 +0100

    refactor: Déplacer la fonction de test de connexion dans sequelize.js et supprimer le fichier testConnection.js

--- api/models/sequelize.js
+++ api/models/sequelize.js
@@ -10,3 +10,16 @@ export const sequelize = new Sequelize(process.env.DB_URL, {
     updatedAt: "updated_at",
   },
 });
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log("✅ Connexion à la base de données réussie !");
+  } catch (error) {
+    console.error("❌ Erreur de connexion :", error);
+  } finally {
+    await sequelize.close();
+  }
+}
+
+testConnection();
